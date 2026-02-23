23c6f3781e7fbf06155d2f56cad07f67534da0ba
kaspernj <kasper@diestoeckels.de>
2026-02-23 19:29:50 +0000

    Add release and module scripts

---
+++ build/row.d.ts
@@ -0,0 +1,5 @@
+declare const _default: React.MemoExoticComponent<(arg0: Record<string, any>) => any>;
+export default _default;
+export const RowContext: React.Context<any>;
+import React from "react";
+//# sourceMappingURL=row.d.ts.map
